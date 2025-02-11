b1189923fb6f566934a29e5329400ae249814111
bl4ze4447 <belutoni06@gmail.com>
2025-02-11 10:47:26 +0100

    Enhance physics and visuals, refactor drawing/updating
    Updated player and pipe movement physics for smoother gameplay and adjusted screen/element sizes. Added a new "slow falling" texture and refactored functions to separate drawing and updating logic. These changes improve readability, maintainability, and gameplay experience.

--- main.c
+++ main.c
@@ -1,20 +1,20 @@
 #include <raylib.h>
 #include <raymath.h>
 #include <stdio.h>
-#include <string.h>
-#include <unistd.h>
 
-#define SCREEN_WIDTH        1280
-#define SCREEN_HEIGHT       720
+#define SCREEN_WIDTH        640
+#define SCREEN_HEIGHT       640
 #define PLAYER_HEIGHT       30
-#define PLAYER_WIDTH        50
+#define PLAYER_WIDTH        40
 #define GRAVITY_FORCE       6       // How many pixels does the player drop per frame
-#define PIPE_WIDTH          150
+#define GRAVITY_ITERATIONS  15
+#define PIPE_WIDTH          100
 #define PIPE_SPACE          (PLAYER_HEIGHT * 7)
-#define PIPE_MOVEMENT       10      // How fast the pipes move towards the left side of the screen
-#define JUMP_FORCE          70      // How many pixels does the player jump
+#define PIPE_MOVEMENT       7      // How fast the pipes move towards the left side of the screen
+#define JUMP_FORCE          80      // How many pixels does the player jump
 #define JUMP_ITERATIONS     10      // divided in this many steps for a visually smoother jump
-                                    // First iteration is doubled, making it JUMP_FORCE + (JUMP_FORCE/JUMP_ITERATIONS)
+                                    // First iteration is doubled, making it
+                                    // JUMP_FORCE*2/JUMP_ITERATIONS + (JUMP_FORCE/JUMP_ITERATIONS)
 
 /* ----------- GAME STRUCTS/ENUMS ----------- */
 enum GameState {
@@ -31,12 +31,15 @@ struct Player {
     Rectangle   rect;
     Texture2D   texture_falling;
     Texture2D   texture_jumping;
+    Texture2D   texture_slow_falling;
 
     double      jump_delay;
     double      last_jump_time;
     int         jump_force;
     int         jump_iterations_left;
     bool        can_jump;
+
+    int         small_gravity_iterations;
 };
 
 struct Pipe {
@@ -57,11 +60,14 @@ Texture2D top_pipe_texture, bottom_pipe_texture;
 int last_pipe_offset;   // Spawn location of pipes that reached the left-side of the screen
 
 /* ----------- FUNCTIONS ----------- */
-/* Draw functions also manage the movement of their parameter (excluded: draw_score) */
-void draw_player(struct Player *);
-void draw_pipe(struct Pipe *);
-void draw_pipes(struct Pipe [], int);
+void draw_player(const struct Player *);
+void draw_pipe(const struct Pipe *);
+void draw_pipes(const struct Pipe [], int);
 void draw_score(int);
+
+void update_player(struct Player *);
+void update_pipe(struct Pipe *);
+void update_pipes(struct Pipe [], int);
 
 /* Handles jump input */
 void handle_input(struct Player *);
@@ -109,14 +115,19 @@ int main() {
             // first is big jump
             player.rect.y -= JUMP_FORCE / (player.jump_iterations_left == JUMP_ITERATIONS ? JUMP_ITERATIONS/2.f : JUMP_ITERATIONS);
             --player.jump_iterations_left;
+
+            if (!player.jump_iterations_left)
+                player.small_gravity_iterations = GRAVITY_ITERATIONS;
         }
 
-        const bool collided = check_collisions(&player, pipes, pipe_count);
+
+        update_player(&player);
+        update_pipes(pipes, pipe_count);
+
+        if (check_collisions(&player, pipes, pipe_count))
+            game_info.state = DIED;
 
         BeginDrawing();
-
-        if (collided)
-            game_info.state = DIED;
 
         ClearBackground(WHITE);
         DrawTexture(bg_texture, 0, 0, WHITE);
@@ -134,21 +145,47 @@ int main() {
 }
 
 
-void draw_player(struct Player * player) {
-    if (player == NULL) return;
+void draw_player(const struct Player * player) {
+    if (player == NULL)
+        return;
 
-    if (player->rect.y < 0) player->rect.y = 0;
-    else if (player->rect.y + player->rect.height > SCREEN_HEIGHT) {
-        game_info.state = DIED;
-    }
-
-    if (player->jump_iterations_left)
+    if (player->jump_iterations_left) {
         DrawTexture(player->texture_jumping, (int)player->rect.x, (int)player->rect.y, WHITE);
-    else
+    }
+    else if (player->small_gravity_iterations) {
+        DrawTexture(player->texture_slow_falling, (int)player->rect.x, (int)player->rect.y, WHITE);
+    }
+    else {
         DrawTexture(player->texture_falling, (int)player->rect.x, (int)player->rect.y, WHITE);
+    }
 }
 
-void draw_pipe(struct Pipe * pipe) {
+void draw_pipe(const struct Pipe * pipe) {
+    if (pipe == NULL)
+        return;
+
+    DrawTexture(top_pipe_texture, (int)pipe->x, (int)(pipe->y_up - SCREEN_HEIGHT + pipe->height_up), WHITE);
+    DrawTexture(bottom_pipe_texture, (int)pipe->x, (int)pipe->y_down, WHITE);
+}
+
+void draw_pipes(const struct Pipe pipes[], const int pipe_count) {
+    for (int i = 0; i < pipe_count; i++)
+        draw_pipe(&pipes[i]);
+}
+
+void update_player(struct Player * player) {
+    if (player == NULL) return;
+
+    if (player->rect.y < 0)
+        player->rect.y = 0;
+    else if (player->rect.y + player->rect.height > SCREEN_HEIGHT)
+        game_info.state = DIED;
+}
+
+void update_pipe(struct Pipe * pipe) {
+    if (pipe == NULL)
+        return;
+
     if (!pipe->score_consumed && pipe->x < SCREEN_WIDTH / 2.0 - PLAYER_WIDTH - PIPE_WIDTH) {
         pipe->score_consumed = true;
         game_info.score += 1;
@@ -156,17 +193,15 @@ void draw_pipe(struct Pipe * pipe) {
 
     if (pipe->x < -pipe->width)
         generate_pipe(pipe, last_pipe_offset);
-
-    DrawTexture(top_pipe_texture, (int)pipe->x, (int)(pipe->y_up - SCREEN_HEIGHT + pipe->height_up), WHITE);
-    DrawTexture(bottom_pipe_texture, (int)pipe->x, (int)pipe->y_down, WHITE);
 }
 
-void draw_pipes(struct Pipe pipes[], const int pipe_count) {
+void update_pipes(struct Pipe pipes[], const int pipe_count) {
     for (int i = 0; i < pipe_count; i++) {
-        pipes[i].x -= Clamp(PIPE_MOVEMENT * (float)game_info.score / 10.f, PIPE_MOVEMENT, PIPE_MOVEMENT * 1.5f);
-        draw_pipe(&pipes[i]);
+        pipes[i].x -= Clamp(PIPE_MOVEMENT * (float)game_info.score / 25.f, PIPE_MOVEMENT, PIPE_MOVEMENT * 1.5f);
+        update_pipe(&pipes[i]);
     }
 }
+
 
 void draw_score(const int score) {
     static char buf[32];
@@ -175,6 +210,9 @@ void draw_score(const int score) {
 }
 
 void handle_input(struct Player * player) {
+    if (player == NULL)
+        return;
+
     if (player->can_jump && IsKeyDown(KEY_UP) && GetTime() - player->last_jump_time > player->jump_delay) {
         player->can_jump = false;
         player->jump_iterations_left = JUMP_ITERATIONS;
@@ -186,6 +224,9 @@ void handle_input(struct Player * player) {
 }
 
 bool has_collided(const struct Player * player, const struct Pipe * pipe)  {
+    if (player == NULL)
+        return false;
+
     if (CheckCollisionRecs((Rectangle) {
         pipe->x,
         pipe->y_up,
@@ -221,10 +262,18 @@ void apply_gravity(struct Player * player) {
     if (player == NULL)
         return;
 
-    player->rect.y += (float)GRAVITY_FORCE;
+    if (player->small_gravity_iterations) {
+        player->rect.y += Clamp(GRAVITY_FORCE * (float)game_info.score / 35.f, GRAVITY_FORCE / 2.f, GRAVITY_FORCE * 1.f);
+        --player->small_gravity_iterations;
+    }
+    else
+        player->rect.y += Clamp(GRAVITY_FORCE * (float)game_info.score / 25.f, GRAVITY_FORCE, GRAVITY_FORCE * 1.5f);;
 }
 
 void generate_pipe(struct Pipe * pipe, const int offset) {
+    if (pipe == NULL)
+        return;
+
     pipe->x = SCREEN_WIDTH + offset;
     pipe->width = PIPE_WIDTH;
 
@@ -261,7 +310,8 @@ void init_player(struct Player * player) {
         .can_jump = true,
         .jump_iterations_left = 0,
         .texture_falling = player->texture_falling,
-        .texture_jumping = player->texture_jumping
+        .texture_jumping = player->texture_jumping,
+        .texture_slow_falling = player->texture_slow_falling,
     };
 }
 
@@ -272,10 +322,12 @@ void load_player_texture(struct Player * player) {
     Image player_img = LoadImage("player.png");
     ImageResize(&player_img, PLAYER_WIDTH, PLAYER_HEIGHT);
 
+    player->texture_slow_falling = LoadTextureFromImage(player_img);
+
     ImageRotate(&player_img, -30);
     player->texture_jumping = LoadTextureFromImage(player_img);
 
-    ImageRotate(&player_img, 30);
+    ImageRotate(&player_img, 45);
     player->texture_falling = LoadTextureFromImage(player_img);
 
     UnloadImage(player_img);
